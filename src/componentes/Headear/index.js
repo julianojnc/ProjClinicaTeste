ada5b6fd9c2f017bfdd5164bd8fe06560e710d66
julianojnc <100528900+julianojnc@users.noreply.github.com>
2024-01-24 09:03:28 -0300

    Alterando componente da Page Home, estilo da Page Home

--- src/componentes/Headear/index.js
+++ src/componentes/Headear/index.js
@@ -1,5 +1,5 @@
 import "./Header.css"
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import React, { useState, useEffect } from "react";
 import MenuLink from "componentes/MenuLink";
 
--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -12,11 +12,11 @@ const PageHome = () => {
 
       <section className="clinic-info">
         <section className="clinic-info_description">
+        <h3>Algo aqui</h3>
           <article className="desc_img">
             <img src={Womam} alt="Veterinary white woman with a dog" />
           </article>
-          <article className="clinic-info_desc_txt">
-            <h3>Algo aqui</h3>
+          <article className="clinic-info_desc_txt">            
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis minus, officia quis aspernatur
               voluptatibus, praesentium similique nisi, sint eos necessitatibus quod asperiores pariatur. Ab
               expedita porro explicabo veritatis libero consectetur!</p>
@@ -32,11 +32,11 @@ const PageHome = () => {
 
         </section>
         <section id="flex-column-reverse" className="clinic-info_description">
+        <h3>Equipe qualificada</h3>
           <article className="desc_img">
             <img src={Equipe} alt="Imagem de uma equipe de veterinários" />
           </article>
           <article className="clinic-info_desc_txt">
-            <h3>Equipe qualificada</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis minus, officia quis aspernatur
               voluptatibus, praesentium similique nisi, sint eos necessitatibus quod asperiores pariatur. Ab
               expedita porro explicabo veritatis libero consectetur!</p>
@@ -52,11 +52,11 @@ const PageHome = () => {
         </section>
 
         <section className="clinic-info_description">
+        <h3>Seu pet em <i>casa</i></h3>
           <article className="desc_img">
             <img src={EmCasa} alt="Pet se sentindo em casa" />
           </article>
-          <article className="clinic-info_desc_txt">
-            <h3>Seu pet em <i>casa</i></h3>
+          <article className="clinic-info_desc_txt">            
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis minus, officia quis aspernatur
               voluptatibus, praesentium similique nisi, sint eos necessitatibus quod asperiores pariatur. Ab
               expedita porro explicabo veritatis libero consectetur!</p>
