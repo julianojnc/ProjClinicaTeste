3fb5a710a3b3d604390878d85866d3dcdec5e4c0
julianojnc <100528900+julianojnc@users.noreply.github.com>
2024-02-02 09:46:54 -0300

    Page About

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -1,83 +1,25 @@
-
+import './PageSobre.css'
+import Clinica from 'assets/img/Frame 8.png';
+import TimeLine from 'assets/img/Frame 11.png';
 
 const PageSobre = () => {
     return (
         <main>
-            <section class="teste">
+            <section className='about-container'>
+                <img src={Clinica} alt='imagem' />
 
-                <h1>Sobre Page</h1>
+                <div className='about-container-content'>
+                    <h3>Nossa História</h3>
+                    <p>
+                        Uma Década de Dedicação: A História de Nossa Clínica Veterinária<br /><br />
 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
-                <br /><br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
-                <br /><br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
-                <br /><br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
-                <br /><br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
-                <br /><br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
-                <br /><br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
-                <br /><br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
-                <br /><br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia
-                neque aperiam temporibus iure assumenda totam ratione fugit, impedit vero
-                sunt animi! Sed, quaerat odit! Repellendus quidem minus molestias soluta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio in
-                consequuntur. Odio sint ipsa, unde praesentium dignissimos autem eveniet
-                cumque eos suscipit deleniti, inventore pariatur, optio iure facere
-                repellendus?
+                        Há mais de 10 anos, nossa clínica veterinária abriu as portas com um propósito simples:<br />
+                        cuidar dos animais com paixão e expertise. Desde então, enfrentamos desafios e crescemos, expandindo nossos serviços e construindo uma reputação de excelência. Com uma equipe dedicada e apaixonada, oferecemos cuidados excepcionais, desde consultas de rotina até cirurgias complexas. Ao longo dos anos, testemunhamos inúmeras histórias emocionantes, sempre com o bem-estar dos animais em primeiro lugar. Enquanto celebramos esta década de serviço, olhamos para o futuro com esperança e determinação, prontos para continuar nossa missão de amor e cuidado pelos animais.
+                    </p>
+
+                    <img src={TimeLine} alt='imagem' />
+                </div>
+
             </section>
         </main>
     )
