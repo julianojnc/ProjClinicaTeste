4e477fd51c00318281fcd53ffd31338319ba0da3
julianojnc <100528900+julianojnc@users.noreply.github.com>
2024-02-02 16:43:06 -0300

    TimeLine na pagina sobre

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -1,6 +1,7 @@
 import './PageSobre.css'
 import Clinica from 'assets/img/Frame 8.png';
-import TimeLine from 'assets/img/Frame 11.png';
+import Timelineimg from 'assets/img/Frame 11.png';
+import TimeLine from 'componentes/TimeLine';
 
 const PageSobre = () => {
     return (
@@ -9,15 +10,19 @@ const PageSobre = () => {
                 <img src={Clinica} alt='imagem' />
 
                 <div className='about-container-content'>
-                    <h3>Nossa História</h3>
-                    <p>
-                        Uma Década de Dedicação: A História de Nossa Clínica Veterinária<br /><br />
+                    <div className='content'>
 
-                        Há mais de 10 anos, nossa clínica veterinária abriu as portas com um propósito simples:<br />
-                        cuidar dos animais com paixão e expertise. Desde então, enfrentamos desafios e crescemos, expandindo nossos serviços e construindo uma reputação de excelência. Com uma equipe dedicada e apaixonada, oferecemos cuidados excepcionais, desde consultas de rotina até cirurgias complexas. Ao longo dos anos, testemunhamos inúmeras histórias emocionantes, sempre com o bem-estar dos animais em primeiro lugar. Enquanto celebramos esta década de serviço, olhamos para o futuro com esperança e determinação, prontos para continuar nossa missão de amor e cuidado pelos animais.
-                    </p>
+                        <h3>Nossa História</h3>
+                        <p>
+                            Uma Década de Dedicação: A História de Nossa Clínica Veterinária<br /><br />
 
-                    <img src={TimeLine} alt='imagem' />
+                            Há mais de 10 anos, nossa clínica veterinária abriu as portas com um propósito simples:<br />
+                            cuidar dos animais com paixão e expertise. Desde então, enfrentamos desafios e crescemos, expandindo nossos serviços e construindo uma reputação de excelência. Com uma equipe dedicada e apaixonada, oferecemos cuidados excepcionais, desde consultas de rotina até cirurgias complexas. Ao longo dos anos, testemunhamos inúmeras histórias emocionantes, sempre com o bem-estar dos animais em primeiro lugar. Enquanto celebramos esta década de serviço, olhamos para o futuro com esperança e determinação, prontos para continuar nossa missão de amor e cuidado pelos animais.
+                        </p>
+                    </div>
+                    <div className='content-time-line'>
+                        <TimeLine />
+                    </div>
                 </div>
 
             </section>
