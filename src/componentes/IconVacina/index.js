6707ca420e389567b0d053702a29555a61a17a45
julianojnc <100528900+julianojnc@users.noreply.github.com>
2024-02-15 19:00:34 -0300

    Adcao Pagina NotFound, reformulacao icones svg

--- src/componentes/IconVacina/index.js
+++ src/componentes/IconVacina/index.js
@@ -4,7 +4,22 @@ const IconVacina = () => {
     return (
         <li className='infocardsicon'>
             <i className="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="none" stroke="lightblue" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m17 3l4 4m-2-2l-4.5 4.5m-3-3l6 6m-1-1L10 18H6v-4l6.5-6.5m-5 5L9 14m1.5-4.5L12 11M3 21l3-3" /></svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={100}
+                    height={100}
+                    viewBox="0 0 24 24"
+
+                >
+                    <path
+                        fill="none"
+                        stroke="#add8e6"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M17 3l4 4m-2-2l-4.5 4.5m-3-3l6 6m-1-1L10 18H6v-4l6.5-6.5m-5 5L9 14m1.5-4.5L12 11M3 21l3-3"
+                    />
+                </svg>
             </i>
         </li>
 
